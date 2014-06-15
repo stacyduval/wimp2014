9e17a6c20c3368092043cfca7a2681ab25be1d27
Cole Geissinger <colegeissinger@gmail.com>
2014-06-15 00:20:03 -0700

    The start of the header

--- header.php
+++ header.php
@@ -6,20 +6,38 @@
  *
  * @package wimp
  */
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<?php wp_head(); ?>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
+	<!--[if lt IE 8]>
+		<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+
+	<section id="header-toolbar" class="fixed-top">
+		<aside id="slogan">
+			<h4><?php echo esc_html( bloginfo( 'description' ) ); ?></h4>
+		</aside>
+		<aside id="member-wrapper">
+			
+		</aside>
+	</section>
+
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'wimp' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
