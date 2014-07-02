520c5ad2ade693cc9b0baa7166090a20a01183e6
Cole Geissinger <colegeissinger@gmail.com>
2014-07-01 23:41:55 -0700

    Adding homepage map content

--- home-map.php
+++ home-map.php
@@ -1,3 +1,18 @@
 <section id="location-map">
-	MAP
+	<h3>Local Chapters</h3>
+
+	<div class="two-column">
+		<!-- MAP IMAGE HERE -->
+		<img src="#" alt="" />
+	</div>
+	<div class="two-column">
+		<!-- Data controlled by Meetup API -->
+		<a class="btn full" href="#"><strong>2</strong> Cities</a>
+		<a class="btn full" href="#"><strong>452</strong> Wimps</a>
+
+		<p><a href="#">WIMP Sonoma County</a> 421 Members<br />
+           <a href="#">WIMP Denver</a> 31 Members</p>
+
+		<p><a href="#">View All</a></p>
+	</div>
 </section>
