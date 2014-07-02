9ac288f7f254945fda763fd8626ba9c98edb0575
Cole Geissinger <colegeissinger@gmail.com>
2014-07-01 23:36:41 -0700

    Adding content to homepage featured block

--- home-featured.php
+++ home-featured.php
@@ -1,3 +1,7 @@
 <section id="feature-block" class="full-bg">
-	FEATURED!
+	<h2>WIMP is a <span class="callout">Global</span> collective of <span class="callout">Local</span> Communities<br />of web & digital media professionals.</h2>
+
+	<div class="description">
+		<h4>We are industry experts and students alike with a common goal to educate, be educated and help cultivate each other’s careers.</h4>
+	</div>
 </section>
