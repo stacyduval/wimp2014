48f97673243af4e95904a71d5986dd3c4fc95fb8
Cole Geissinger <colegeissinger@gmail.com>
2014-10-13 22:00:50 -0700

    Adding dynamic magnification file loading in resources

--- functions.php
+++ functions.php
@@ -98,12 +98,14 @@ add_action( 'widgets_init', 'wimp_widgets_init' );
 /**
  * Enqueue scripts and styles.
  */
-function wimp_scripts() {
-	wp_enqueue_style( 'wimp-style', get_stylesheet_uri() );
+function wimp_scripts( $debug ) {
+	$min = ( $debug || ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ) ? '' : '.min';
 
-	wp_enqueue_script( 'wimp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	// Styles
+	wp_enqueue_style( 'wimp-style', WIMP_ASSETS . "css/wimp{$min}.css", array(), WIMP_VERSION );
 
-	wp_enqueue_script( 'wimp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	// Scripts
+	wp_enqueue_script( 'wimp-scripts', WIMP_ASSETS . "js/wimp{$min}.js", array(), WIMP_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
